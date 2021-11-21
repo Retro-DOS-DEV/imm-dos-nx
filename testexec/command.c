e607c781c86bad9a8cd302618b86057a9caaf004
Andrew Imm <aimm22@gmail.com>
2021-11-20 20:45:30 -0800

    Allow changing current drive from command line

--- testexec/command.c
+++ testexec/command.c
@@ -5,6 +5,9 @@ typedef struct strptr {
   int length;
 } strptr;
 
+const int stdin = 0;
+const int stdout = 1;
+
 int syscall(int method, int arg0, int arg1, int arg2) {
   register int eax asm ("eax") = method;
   register int ebx asm ("ebx") = arg0;
@@ -57,13 +60,44 @@ int get_current_drive_name(char *buffer) {
   return syscall(0x22, (int)buffer, 0, 0) & 7;
 }
 
+int change_drive(strptr *drive_name) {
+  return syscall(0x21, (int)drive_name, 0, 0);
+}
+
 static char readbuffer[512];
+static int current_drive_number = 0x80;
+
+void run(int command_end) {
+  // check for matching builtins
+
+  // check for drive switch command
+  if (readbuffer[command_end - 1] == ':') {
+    strptr name_ptr;
+    name_ptr.addr = (int) readbuffer;
+    name_ptr.length = command_end - 1;
+
+    current_drive_number = change_drive(&name_ptr);
+    return;
+  }
+
+  // assume the command is an attempt to run an executable
+  int id = fork();
+  if (id == 0) {
+    strptr path_ptr;
+    path_ptr.addr = (int) readbuffer;
+    path_ptr.length = command_end;
+
+    exec(&path_ptr, 0);
+    
+    write_file(stdout, "\nFailed to execute program\n");
+    terminate(1);
+  } else {
+    wait(id);
+    write_file(stdout, "\nExited, resuming...\n");
+  }
+}
 
 void _start() {
-  // assume handles 0, 1, 2 are already established
-  int stdin = 0;
-  int stdout = 1;
-
   char current_drive_name[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
   int current_drive_name_length;
 
@@ -71,9 +105,9 @@ void _start() {
     current_drive_name_length = get_current_drive_name(current_drive_name);
     // print drive
     syscall(0x13, stdout, (int)(current_drive_name), current_drive_name_length);
-    write_file(stdout, ":");
+    write_file(stdout, ":\\");
     // write cwd
-
+    
     // print prompt
     write_file(stdout, "> ");
     int bytes_read = read_file(stdin, readbuffer, 512);
@@ -83,19 +117,6 @@ void _start() {
         break;
       }
     }
-    int id = fork();
-    if (id == 0) {
-      strptr path_ptr;
-      path_ptr.addr = (int) readbuffer;
-      path_ptr.length = command_end;
-
-      exec(&path_ptr, 0);
-      
-      write_file(stdout, "\nFailed to execute program\n");
-      terminate(1);
-    } else {
-      wait(id);
-      write_file(stdout, "\nExited, resuming...\n");
-    }
+    run(command_end);
   }
 }
